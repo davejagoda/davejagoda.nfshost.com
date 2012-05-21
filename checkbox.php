9db5eadffc924383150a39a7931061bfc24229e2
Dave Jagoda <davejagoda@gmail.com>
2012-05-21 13:00:45 -0700

    update checkbox.php and checkbox.html again

--- checkbox.php
+++ checkbox.php
@@ -1,1 +1,8 @@
-The following were checked: <?php foreach ($_POST as $array) { foreach ($array as $element) { echo htmlspecialchars($element); } } ?>
+The following were checked:
+<?php
+  foreach ($_POST as $array) {
+    foreach ($array as $element) {
+      echo htmlspecialchars($element);
+    }
+  }
+?>
