38b700f34cd497924da710c5319cd2be6a40c58d
Dave Jagoda <davejagoda@gmail.com>
2012-05-22 09:53:48 -0700

    change dynamic.php to print out the entire  structure, take 2

--- dynamic.php
+++ dynamic.php
@@ -4,10 +4,8 @@
  </head>
  <body>
  <?php
-  foreach ($_SERVER as $array) {
-    foreach ($array as $element) {
-      echo htmlspecialchars($element);
-    }
+  foreach ($_SERVER as $element) {
+    echo htmlspecialchars($element);
   }
 ?>
 
