853b3144fdd53fb38066f1372b1c1e1ab5c19427
Dave Jagoda <davejagoda@gmail.com>
2012-05-22 10:54:40 -0700

    ugly hack, refactor this into a function

--- dynamic.php
+++ dynamic.php
@@ -5,12 +5,21 @@
  <body>
  <?php
   foreach ($_SERVER as $key => $value) {
+   if is_array($value) {
+    foreach ($value as $k => $v) {
+     echo htmlspecialchars($k);
+     echo ":";
+     echo htmlspecialchars($v);
+     echo "<br>";
+    }
+   else {
     echo htmlspecialchars($key);
     echo ":";
     echo htmlspecialchars($value);
     echo "<br>";
+   }
   }
-?>
+ ?>
 
 </body>
 </html>
