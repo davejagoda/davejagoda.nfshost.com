beb373a468dcbf3bdad98d9d7cb7a24937a62c86
Dave Jagoda <davejagoda@gmail.com>
2012-10-24 22:53:24 -0700

    remove the php parts

--- dynamic.py
+++ dynamic.py
@@ -13,22 +13,5 @@ print '''Content-type: text/html
 '''
 for k in sorted(os.environ):
     print "%s:%s<br>" % (escape(k), escape(os.environ[k]))
-# <?php
-#  foreach ($_SERVER as $key => $value) {
-#   if (is_array($value)) {
-#    foreach ($value as $k => $v) {
-#     echo htmlspecialchars($k);
-#     echo ":";
-#     echo htmlspecialchars($v);
-#     echo "<br>";
-#    }
-#   } else {
-#    echo htmlspecialchars($key);
-#    echo ":";
-#    echo htmlspecialchars($value);
-#    echo "<br>";
-#   }
-#  }
-# ?>
 print '''</body>
 </html>'''
