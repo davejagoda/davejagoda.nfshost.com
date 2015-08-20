85a0b9837e5fe9d37d434462c5b96312690332e6
Dave Jagoda <davejagoda@gmail.com>
2015-08-20 15:05:59 -0700

    futz around with whitespace in dynamic.php

--- dynamic.php
+++ dynamic.php
@@ -6,44 +6,45 @@
  <br>
  <b>SERVER</b>
  <br>
- <?php
-  ksort($_SERVER);
-  foreach ($_SERVER as $key => $value) {
-   if (is_array($value)) {
-    foreach ($value as $k => $v) {
-     echo htmlspecialchars($k);
-     echo ":";
-     echo htmlspecialchars($v);
-     echo "<br>\n";
-    }
-   } else {
-    echo htmlspecialchars($key);
+<?php
+ ksort($_SERVER);
+ foreach ($_SERVER as $key => $value) {
+  if (is_array($value)) {
+   foreach ($value as $k => $v) {
+    echo htmlspecialchars($k);
     echo ":";
-    echo htmlspecialchars($value);
+    echo htmlspecialchars($v);
     echo "<br>\n";
    }
+  } else {
+   echo htmlspecialchars($key);
+   echo ":";
+   echo htmlspecialchars($value);
+   echo "<br>\n";
   }
- ?>
+ }
+?>
  <br>
  <b>POST</b>
  <br>
- <?php
-  ksort($_POST);
-  foreach ($_POST as $key => $value) {
-   if (is_array($value)) {
-    foreach ($value as $k => $v) {
-     echo htmlspecialchars($k);
-     echo ":";
-     echo htmlspecialchars($v);
-     echo "<br>\n";
-    }
-   } else {
-    echo htmlspecialchars($key);
+<?php
+ ksort($_POST);
+ foreach ($_POST as $key => $value) {
+  if (is_array($value)) {
+   foreach ($value as $k => $v) {
+    echo htmlspecialchars($k);
     echo ":";
-    echo htmlspecialchars($value);
+    echo htmlspecialchars($v);
     echo "<br>\n";
    }
+  } else {
+   echo htmlspecialchars($key);
+   echo ":";
+   echo htmlspecialchars($value);
+   echo "<br>\n";
   }
- ?>
+ }
+?>
+ <br>
 </body>
 </html>
