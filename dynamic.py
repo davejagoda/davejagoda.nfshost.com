34aad6a2ca3b0902ca02a9f7bd953d8fa83308ad
dj <davejagoda@users.noreply.github.com>
2018-10-26 19:02:00 -0700

    Update dynamic.py to use print() and .format

--- dynamic.py
+++ dynamic.py
@@ -3,15 +3,15 @@
 import os
 from cgi import escape
 
-print '''Content-type: text/html
+print('''Content-type: text/html
 
 <html>
  <head>
   <title>dynamic</title>
  </head>
  <body>
-'''
+''')
 for k in sorted(os.environ):
-    print "%s:%s<br>" % (escape(k), escape(os.environ[k]))
-print '''</body>
-</html>'''
+    print('{}:{}<br>'.format(escape(k), escape(os.environ[k])))
+print('''</body>
+</html>''')
