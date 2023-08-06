b120c74bd8eadeefdf09c06c2b2324b61107ce6b
dj <davejagoda+github@gmail.com>
2023-08-05 18:40:59 -0700

    Refactor post.php to print all POST and JSON data

--- post.php
+++ post.php
@@ -1,4 +1,13 @@
-Item was <?php echo htmlspecialchars($_POST['item']); ?>
-<br>
-Elem was <?php echo htmlspecialchars($_POST['elem']); ?>
-<br>
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>POST</title>
+  </head>
+  <body>
+    <p>POST</p>
+    <?php print_r($_POST); ?>
+    <p>JSON</p>
+    <?php print_r(json_decode(file_get_contents('php://input'))); ?>
+  </body>
+</html>
