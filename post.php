215d80f9a9570d3d862445ac89b0f8ecd87752f7
dj <davejagoda+github@gmail.com>
2025-02-23 08:05:26 -0800

    Escape htmlspecialchars in post.php

--- post.php
+++ post.php
@@ -1,13 +1,30 @@
 <!doctype html>
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <title>POST</title>
-  </head>
-  <body>
-    <p>POST</p>
-    <?php print_r($_POST); ?>
-    <p>JSON</p>
-    <?php print_r(json_decode(file_get_contents('php://input'))); ?>
-  </body>
+    <head>
+        <meta charset="utf-8" />
+        <title>POST</title>
+    </head>
+    <body>
+        <p>POST</p>
+        <?php
+        ksort($_POST);
+        foreach ($_POST as $key => $value) {
+            if (is_array($value)) {
+                foreach ($value as $k => $v) {
+                    echo htmlspecialchars($k);
+                    echo ":";
+                    echo htmlspecialchars($v);
+                    echo "<br>\n";
+                }
+            } else {
+                echo htmlspecialchars($key);
+                echo ":";
+                echo htmlspecialchars($value);
+                echo "<br>\n";
+            }
+        }
+        ?>
+        <p>JSON</p>
+        <?php print_r(json_decode(file_get_contents('php://input'))); ?>
+    </body>
 </html>
